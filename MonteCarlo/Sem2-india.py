09734ecc83642b0bce3241ee586770acf2cfd195
anmok160 <93107350+anmok160@users.noreply.github.com>
2022-03-26 23:07:43 +0800

    Update Sem2-india.py

--- MonteCarlo/Sem2-india.py
+++ MonteCarlo/Sem2-india.py
@@ -13,7 +13,7 @@ def GBMsimulator(seed, So, mu, sigma, Cov, T, N):
     N:      number of increments
     """
 
-    np.random.seed(seed)
+    np.random.rand(20)
     dim = np.size(So)
     t = np.linspace(0., T, int(N))
     A = np.linalg.cholesky(Cov)
@@ -29,26 +29,26 @@ def GBMsimulator(seed, So, mu, sigma, Cov, T, N):
 import pandas_datareader as pdr
 from datetime import datetime
 
-intc = pdr.get_data_yahoo(symbols='INTC', start=datetime(2018, 1, 1), end=datetime(2020, 1, 1))
-amd = pdr.get_data_yahoo(symbols='AMD', start=datetime(2018, 1, 1), end=datetime(2020, 1, 1))
+ko = pdr.get_data_yahoo(symbols='KO', start=datetime(2018, 1, 1), end=datetime(2020, 1, 1))
+pep = pdr.get_data_yahoo(symbols='PEP', start=datetime(2018, 1, 1), end=datetime(2020, 1, 1))
 
 #Visualize the closing price history
 import matplotlib.pyplot as plt
 
 plt.figure(figsize = (16,8))
 plt.title('Close Price History', fontsize = 18)
-plt.plot(intc['Adj Close'])
-plt.plot(amd['Adj Close'])
-plt.legend(['INTC', 'AMD'], loc = 'upper left', fontsize = 18)
+plt.plot(ko['Adj Close'])
+plt.plot(pep['Adj Close'])
+plt.legend(['KO', 'PEP'], loc = 'upper left', fontsize = 18)
 plt.xlabel('Date', fontsize = 18)
 plt.ylabel('Close Price USD ($)', fontsize = 18)
 plt.show()
 
 # Two-dimensional Case
 
-seed = 22                       
+seed = 50                       
 
-dim = 2; T = 1; N = int(2.**9)
+dim = 2; T = 252; N = int(2.**9)
 
 S0 = np.array([100, 100])
 
@@ -56,22 +56,22 @@ S0 = np.array([100, 100])
 
 # logarithmic returns
 
-intc['log_returns'] = np.log(intc['Adj Close']).diff()
-amd['log_returns'] = np.log(amd['Adj Close']).diff()
+ko['log_returns'] = np.log(ko['Adj Close']).diff()
+pep['log_returns'] = np.log(pep['Adj Close']).diff()
 
 # expected returns
 
-mean_intc = intc['log_returns'][1:].mean()
-mean_amd = amd['log_returns'][1:].mean()
-mu = [mean_intc, mean_amd]
+mean_ko = ko['log_returns'][1:].mean()
+mean_pep = pep['log_returns'][1:].mean()
+mu = [mean_ko, mean_pep]
 
 # volatility
-std_intc = intc['log_returns'][1:].std()
-std_amd = amd['log_returns'][1:].std()
-sigma = np.array([std_intc, std_amd])
+std_ko = ko['log_returns'][1:].std()
+std_pep = pep['log_returns'][1:].std()
+sigma = np.array([std_ko, std_pep])
 
 # covariance matrix
-Cov = np.cov(intc['log_returns'][1:], amd['log_returns'][1:] )
+Cov = np.cov(ko['log_returns'][1:], pep['log_returns'][1:] )
 
 stocks, time = GBMsimulator(seed, S0, mu, sigma, Cov, T, N)
 
@@ -87,52 +87,112 @@ plt.xlabel('Time', fontsize = 18)
 plt.legend(['Stock 1', 'Stock 2'], loc = 'upper left', fontsize = 18)
 plt.show()
 
-# Multidimensional Case
-from scipy.stats import random_correlation
+# plot the spread
+plt.figure(figsize=(8, 6), dpi=200)
+plt.plot(stocks[0,:] - stocks[1,:], label = 'Spread (Stock 1 - Stock 2)')
+plt.legend()
+plt.title("Spread between Stock 1 and Stock 2")
+plt.show()
 
-seed = 2222                       
+# Check out the cointegration value: Null hyp. = no cointegration
+import statsmodels.tsa.stattools as ts 
+result = ts.coint(stocks[0,:], stocks[1,:])
 
-dim = 10
+# Cointegration test: A technique used to find a potential correlation in a time series (long term)
+# Determines if the spread between the two assets are constant over time.
+# Null Hypothesis: Spread between series are non-stationary.
+# Uses the augmented Engle-Granger two-step cointegration test.
+cointegration_t_statistic = result[0]
+p_val = result[1]
+critical_values_test_statistic_at_1_5_10 = result[2]
+print('We want the P val < 0.05 (meaning that cointegration exists)')
+print('P value for the augmented Engle-Granger two-step cointegration test is', p_val)
 
-T = 1
-N = int(2.**9)
-S0 = np.random.normal(100, 1, dim)
-mu = np.random.uniform(0.01, 0.01, dim)
-sigma = np.random.uniform(0.01, 0.1, dim)
+from statsmodels.tsa.stattools import adfuller
+# Compute the ADF test for Berkshire Hathaway and Microsoft
+# With all time series, you want to have stationary data otherwise our data will be very hard to predict.
+# ADF for Berkshire Hathaway Class B
+BRK_B_ADF = adfuller(stocks[0,:])
+print('P value for the Augmented Dickey-Fuller Test is', BRK_B_ADF[1])
+MSFT_ADF = adfuller(stocks[1,:])
+print('P value for the Augmented Dickey-Fuller Test is', MSFT_ADF[1])
+Spread_ADF = adfuller(stocks[0,:] - stocks[1,:])
+print('P value for the Augmented Dickey-Fuller Test is', Spread_ADF[1])
+Ratio_ADF = adfuller(stocks[0,:] / stocks[1,:])
+print('P value for the Augmented Dickey-Fuller Test is', Ratio_ADF[1])
+# Spread looks fine. If you'd want even better results, consider taking the difference (order 1) of Berkshire and MSFT
 
-# Random Correlated Geometric Bronian Motion
-if dim == 1:
-    corr = np.array([1])
-else:
-    eig = np.random.uniform(0.1, 1, dim)
-    eig = eig/np.sum(eig)*dim                        # Sum of eigenvalues must equal dimensionality.
-    corr = random_correlation.rvs(eig)
+# Results: can only claim stationary for the spread (since P value < 0.05). This suggests a constant mean over time.
+# Therefore, the two series are cointegrated.
 
-Cov = np.tensordot(sigma, sigma, 0) * corr           # covariance matrix
 
-# Uncorrelated Geometric Bronian Motion
-#corr = np.identity(dim)
+# Also, we can take a look at the price ratios between the two time series.
+import pandas as pd
+plt.figure(figsize=(8, 6), dpi=200)
+ratio = stocks[0,:] / stocks[1,:]
+plt.plot(ratio, label = 'Price Ratio (stock1 / stock2))')
+plt.axhline(ratio.mean(), color='red')
+plt.legend()
+plt.title("Price Ratio between stock1 and stock2")
+plt.show()
 
-stocks, time = GBMsimulator(seed, S0, mu, sigma, Cov, T, N)
-# Number of Simulations
+# NOTE, here you can either use the spread OR the Price ratio approach. Anyways, let's standardize the ratio so we can have a 
+# upper and lower bound to help evaluate our trends.. Let's stick with the ratio data.
+plt.figure(figsize=(8, 6), dpi=200)
+# Calculate the Zscores of each row.
+df_zscore = (ratio - ratio.mean())/ratio.std()
+plt.plot(df_zscore, label = "Z Scores")
+plt.axhline(df_zscore.mean(), color = 'black')
+plt.axhline(1.0, color='red') # Setting the upper and lower bounds to be the z score of 1 and -1 (1/-1 standard deviation)
+plt.axhline(1.25, color='red') # 95% of our data will lie between these bounds.
+plt.axhline(-1.0, color='green') # 68% of our data will lie between these bounds.
+plt.axhline(-1.25, color='green') # 95% of our data will lie between these bounds.
+plt.legend(loc = 'best')
+plt.title('Z score of Ratio of stock1 to stock2')
+plt.show()
+# For the most part, the range that exists outside of these 'bands' must come converge back to the mean. Thus, you can 
+# determine when you can go long or short the pair (BRK_B to MSFT).
 
-np.random.seed(seed)
-simul = int(10)                                       
 
-SS = np.zeros([simul, dim, N])
+plt.figure(figsize=(8, 6), dpi=200)
+ratio1 = stocks[0,:] / stocks[1,:]
+ratio = pd.DataFrame(ratio1)
+ratios_mavg5 = ratio.rolling(window=5, center=False).mean()
+ratios_mavg20 = ratio.rolling(window=20, center=False).mean()
+std_20 = ratio.rolling(window=20, center=False).std()
+zscore_20_5 = (ratios_mavg5 - ratios_mavg20)/std_20
+plt.plot(ratio.index, ratio.values)
+plt.plot(ratios_mavg5.index, ratios_mavg5.values)
+plt.plot(ratios_mavg20.index, ratios_mavg20.values)
+plt.legend(['Ratio', '5d Ratio MA', '20d Ratio MA'])
+plt.xlabel('Date')
+plt.ylabel('Ratio')
+plt.show()
 
-SS[0, :, :] = stocks
 
-for k in range(1, simul):
-    seed = int(np.random.uniform(1,2**32-1,1))
-    SS[k, :, :] = GBMsimulator(seed, S0, mu, sigma, Cov, T, N)[0]
+plt.figure(figsize=(8, 6))
+zscore_20_5.plot()
+plt.axhline(0, color='black')
+plt.axhline(1, color='red', linestyle='--')
+plt.axhline(1.25, color='red', linestyle='--')
+plt.axhline(-1, color='green', linestyle='--')
+plt.axhline(-1.25, color='green', linestyle='--')
+plt.title("123")
+plt.legend(['Rolling Ratio z-score', 'Mean', '+1','+1.25','-1','-1.25'])
+plt.show()
 
-# Plot one of the simulations, e.g. the 0th
 
-plt.figure(figsize = (16,8))
-plt.title('Multidimensional Correlated GBM', fontsize = 18)
-plt.xlabel('Time', fontsize = 18)
 
-for j in range(dim):
-    plt.plot(time, SS[0, j, :])
+plt.figure(figsize=(8, 6), dpi=200)
+ratio.plot()
+buy = ratio.copy()
+sell = ratio.copy()
+buy[zscore_20_5>-1] = 0
+sell[zscore_20_5<1] = 0
+buy.plot(color='g', linestyle='None', marker='^')
+sell.plot(color='r', linestyle='None', marker='^')
+x1, x2, y1, y2 = plt.axis()
+plt.axis((x1, x2, ratio.min(), ratio.max()))
+plt.legend(['Ratio', 'Buy Signal', 'Sell Signal'])
+plt.title('Relationship BRK to MSFT')
 plt.show()
